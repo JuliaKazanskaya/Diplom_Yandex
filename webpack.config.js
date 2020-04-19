677ce7d71a2a41baef59c9d320eee797751cadfb
Julia Bolelova <bolelova.julia@gmail.com>
2020-04-19 04:53:40 +0300

    add main pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,112 @@
-const path = require('path'); 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Подключили к проекту плагин
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // научился работать с HTML
-const WebpackMd5Hash = require('webpack-md5-hash'); // Отслеживает хеши и обновляет их
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 const webpack = require('webpack');
-
-// создаем переменную для development-сборки
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  // Точка входа (откуда брать файл)
-  entry: { main: './src/js/index.js' },
-  // Точка выхода (куда будут записываться фалы)
+  entry: {
+    main: './src/index.js',
+    about: './src/about/about.js',
+    analytics: './src/analytics/analytics.js'
+  },
   output: {
-  path: path.resolve(__dirname, 'dist'),
-  filename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/[name].[chunkhash].js'
   },
-  
-  // Правила обработки файлов при сборке
   module: {
-  rules: [ // тут описываются правила
-    {
-    test: /\.js$/, // регулярное выражение, которое ищет все js файлы
-    use: { loader: 'babel-loader' }, // весь JS обрабатывается пакетом babel-loader
-    exclude: /node_modules/ // исключает папку node_modules
-    },
-    {
-    test: /\.css$/i, // применять это правило только к CSS-файлам
-    use: [(isDev ? 'style-loader' : MiniCssExtractPlugin.loader), // в правилах укажите, что если вы собираете в режиме dev, то плагин MiniCssExtractPlugin загружать не нужно.
-      'css-loader', 
-      'postcss-loader'
-    ]
-    },
-    {
-    test: /\.(png|jpg|gif|ico|svg)$/,
-    use: [
-      {
-      loader: 'file-loader',
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
       options: {
-        name: './images/[name].[ext]', // указали папку, куда складывать изображения
-        esModule: false
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-proposal-class-properties"]
       }
-      },
-      {
-      loader: 'image-webpack-loader',
-      options: {}
-      },
-    ]
     },
-    
-    // Настройка для подгрузки шрифтов
     {
-    test: /\.(eot|ttf|woff|woff2)$/,
-    loader: 'file-loader',
-    options: {
-      name: './vendor/[name].[ext]', // указали папку, куда складывать шрифты
-    }
-    }
-  ]
-  },
+      test: /\.css$/,
+      use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+              publicPath: '../'
+          }
+      },
+          {
+              loader: 'css-loader',
+              options: {
+                  importLoaders: 1
+              },
+          },
 
-  plugins: [ 
-  new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
-  new OptimizeCssAssetsPlugin({
-    assetNameRegExp: /\.css$/g,
-    cssProcessor: require('cssnano'),
-    cssProcessorPluginOptions: {
-      preset: ['default'],
+          {
+              loader: 'postcss-loader',
+              options: {
+                  config: {
+                      path: __dirname + '/postcss.config.js'
+                  }
+              },
+          },
+      ],
+  },
+    {
+      test: /\.(png|jpe?g|gif|ico|svg)$/,
+      use:
+        [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+            }
+          }
+        ],
     },
-    canPrint: true
-  }), // подключите плагин после MiniCssExtractPlugin
-  new HtmlWebpackPlugin({
-    // Означает, что:
-    inject: false, // стили НЕ нужно прописывать внутри тегов
-    template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
-    filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-  }),
-  new WebpackMd5Hash(),
-  new webpack.DefinePlugin({
-    'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  })
+    {
+      test: /\.(eot|ttf|woff|woff2|otf)$/,
+      loader: 'file-loader?name=./vendor/fonts/[name].[ext]'
+    },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '.[name].[contenthash].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/about/about.html',
+      filename: 'about.html'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/analytics/analytics.html',
+      filename: 'analytics.html'
+    }),
+    new WebpackMd5Hash(),
+    new webpack.DefinePlugin({
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    })
   ]
 };
